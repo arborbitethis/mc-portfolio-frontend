4b4c5e7d4bade33284d3659857d001ffcf56a1b7
arborbitethis <thew4yew@gmail.com>
2024-02-28 10:01:12 -0500

    Removed link to broken recipes page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,15 +14,6 @@ function NavBar() {
             <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
           </Link>
         </li>
-        <li className="mx-8 group">
-          <Link
-            to="/recipes"
-            className="text-gray-700 font-bold no-underline pb-1 relative block transition-colors duration-300 hover:text-gray-500"
-          >
-            Recipes
-            <span className="absolute left-0 bottom-0 h-0.5 w-0 bg-gray-700 transition-all duration-300 group-hover:w-full"></span>
-          </Link>
-        </li>
       </ul>
     </nav>
   );
